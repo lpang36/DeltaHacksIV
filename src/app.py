e9d2840e14971f16e3ec60b946f2e756644d0c2e
Lawrence Pang <lawrencepang36@gmail.com>
2018-01-28 10:11:53 -0500

    work on front end

--- src/app.py
+++ src/app.py
@@ -1,6 +1,5 @@
-from flask import Flask,request, Response, jsonify,abort
+from flask import Flask,request, Response, jsonify,abort, render_template
 import json
-import fastAStar
 from path_plan import path_plan
 
 
@@ -8,21 +7,15 @@ app = Flask(__name__)
 
 app.config.update(JSONIFY_PRETTYPRINT_REGULAR=False)
 
+@app.route('/')
+def __main__():
+  return render_template('index.html')
+
 @app.route('/api/<min_x>/<min_y>/<max_x>/<max_y>', methods = ['GET'])
 def api(min_x,min_y,max_x,max_y):
-    data = path_plan((float(min_x),float(min_y)),(float(max_x),float(max_y)))
-    print(data)
-    #var = [var for var in data if var['id']== choice]
-
-    #if(len(var) == 0):
-    #    abort(404)
-    return jsonify({'path':data})
-
-
-    """resp = jsonify(data)
-    resp.status_code = 200
-    return resp"""
-
+  data = path_plan((float(min_x),float(min_y)),(float(max_x),float(max_y)))
+  print(data)
+  return jsonify({'path':data})
 
 if __name__ == "__main__":
-    app.run()
+  app.run()
